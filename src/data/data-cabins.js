e105ea9e728570e23f142f9b8085732644d9105e
Adrian Prajsnar <adrian.prajsnar11@gmail.com>
2024-09-08 09:38:45 +0200

    Update cabins descriptions

--- src/data/data-cabins.js
+++ src/data/data-cabins.js
@@ -10,7 +10,7 @@ export const cabins = [
     discount: 10,
     image: imageUrl + 'cabin-001.jpg',
     description:
-      "Escape to the heart of the enchanted forest and discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a magical setting, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace, and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens, guarantees a peaceful night's sleep. Relax in the spa-like shower and unwind on the private deck with a hot tub surrounded by the whispering trees.",
+      "Retreat to the core of the mystical forest and experience the pinnacle of luxury for couples in the charming wooden Cabin 001. Tucked away in a spellbinding environment, this exquisite cabin provides a secluded and romantic escape. Inside, you'll find elegant wooden interiors, a cozy seating area, a fireplace, and a fully-stocked kitchen. The sumptuous king-size bed, adorned with premium linens, promises a restful night’s sleep. Rejuvenate in the spa-inspired shower and relax on the exclusive deck featuring a hot tub, enveloped by the serene sounds of the surrounding trees.",
   },
   {
     name: '002',
@@ -19,7 +19,7 @@ export const cabins = [
     discount: 0,
     image: imageUrl + 'cabin-002.jpg',
     description:
-      "Immerse yourself in the serenity of nature and indulge in luxury in our cozy cabin 002. Perfect for couples, this cabin offers a secluded and intimate retreat in the heart of an enchanted forest. Inside, you will find warm and inviting interiors crafted from high-quality wood, a comfortable living area, a fireplace, and a fully-equipped kitchen. The luxurious bedroom features a plush king-size bed and spa-like shower. Relax on the private deck with a hot tub and let the forest's magic embrace you.",
+      "Immerse yourself in nature's tranquility and enjoy luxury in our charming Cabin 002. Ideal for couples, this cabin provides a private and intimate escape nestled in the heart of an enchanting forest. Inside, you'll discover welcoming, high-quality wooden interiors, a cozy living space, a fireplace, and a fully-equipped kitchen. The opulent bedroom boasts a plush king-size bed and a spa-like shower. Unwind on the private deck with a hot tub and let the forest’s magic surround you.",
   },
   {
     name: '003',
@@ -28,7 +28,7 @@ export const cabins = [
     discount: 0,
     image: imageUrl + 'cabin-003.jpg',
     description:
-      'Experience the magic of luxury family living in our medium-sized wooden cabin 003. Perfect for families of up to 4 people, this cabin offers a comfortable and inviting space with all modern amenities. Inside, you will find warm and inviting interiors crafted from high-quality wood, a comfortable living area, a fireplace, and a fully-equipped kitchen. The bedrooms feature plush beds and spa-like bathrooms. The cabin has a private deck with a hot tub and outdoor seating area, perfect for creating cherished family memories.',
+      "Discover the enchantment of luxurious family living in our charming medium-sized Cabin 003. Ideal for families of up to four, this cabin provides a cozy and welcoming environment with all the modern comforts. Inside, you'll find inviting wooden interiors, a comfortable living area, a fireplace, and a fully-equipped kitchen. The bedrooms feature plush beds and spa-like bathrooms. The cabin also includes a private deck with a hot tub and an outdoor seating area, perfect for making cherished family memories.",
   },
   {
     name: '004',
@@ -37,7 +37,7 @@ export const cabins = [
     discount: 30,
     image: imageUrl + 'cabin-004.jpg',
     description:
-      'Indulge in the ultimate luxury family vacation in this medium-sized cabin 004. Designed for families of up to 4, this cabin offers a sumptuous retreat for the discerning traveler. Inside, the cabin boasts of opulent interiors crafted from the finest quality wood, a comfortable living area, a fireplace, and a fully-equipped gourmet kitchen. The bedrooms are adorned with plush beds and spa-inspired en-suite bathrooms. Step outside to your private deck and soak in the natural surroundings while relaxing in your own hot tub.',
+      'Experience the pinnacle of luxury family vacations in Cabin 004. Tailored for families of up to four, this medium-sized cabin provides an exquisite retreat for the discerning guest. Inside, you’ll find lavish interiors made from the highest quality wood, a cozy living area, a fireplace, and a fully-equipped gourmet kitchen. The bedrooms feature sumptuous beds and spa-inspired en-suite bathrooms. Step outside onto your private deck to enjoy the natural surroundings while unwinding in your own hot tub.',
   },
   {
     name: '005',
@@ -46,7 +46,7 @@ export const cabins = [
     discount: 10,
     image: imageUrl + 'cabin-005.jpg',
     description:
-      'Embark on a comfortable and cozy getaway with your group or family in our spacious cabin 005. Designed to accommodate up to 6 people, this cabin offers a secluded retreat in the heart of nature. Inside, the cabin features warm and inviting interiors crafted from quality wood, a living area with a fireplace, and a fully-equipped kitchen. The bedrooms are comfortable and equipped with en-suite bathrooms. Step outside to your private deck and take in the natural surroundings while relaxing in your own hot tub.',
+      "Set off on a relaxing and snug escape with your group or family in our spacious Cabin 005. Accommodating up to six people, this cabin provides a private retreat immersed in nature. Inside, you'll find warm, inviting interiors crafted from high-quality wood, a living area with a fireplace, and a fully-equipped kitchen. The bedrooms offer comfort and feature en-suite bathrooms. Step outside to your private deck and enjoy the natural surroundings while unwinding in your own hot tub.",
   },
   {
     name: '006',
@@ -55,7 +55,7 @@ export const cabins = [
     discount: 40,
     image: imageUrl + 'cabin-006.jpg',
     description:
-      'Experience the epitome of luxury with your group or family in our spacious wooden cabin 006. Designed to comfortably accommodate up to 6 people, this cabin offers a lavish retreat in the heart of nature. Inside, the cabin features opulent interiors crafted from premium wood, a grand living area with a fireplace, and a fully-equipped gourmet kitchen. The bedrooms are adorned with plush beds and spa-like en-suite bathrooms. Step outside to your private deck and soak in the natural surroundings while relaxing in your own hot tub.',
+      "Discover the height of luxury with your group or family in our expansive wooden Cabin 006. Accommodating up to six people, this cabin provides a sumptuous retreat nestled in nature. Inside, you'll find elegant interiors made from premium wood, a spacious living area with a fireplace, and a fully-equipped gourmet kitchen. The bedrooms feature plush beds and spa-like en-suite bathrooms. Step out onto your private deck to embrace the natural surroundings while enjoying your own hot tub.",
   },
   {
     name: '007',
@@ -64,7 +64,7 @@ export const cabins = [
     discount: 50,
     image: imageUrl + 'cabin-007.jpg',
     description:
-      'Accommodate your large group or multiple families in the spacious and grand wooden cabin 007. Designed to comfortably fit up to 8 people, this cabin offers a secluded retreat in the heart of beautiful forests and mountains. Inside, the cabin features warm and inviting interiors crafted from quality wood, multiple living areas with fireplaces, and a fully-equipped kitchen. The bedrooms are comfortable and equipped with en-suite bathrooms. The cabin has a private deck with a hot tub and outdoor seating area, perfect for taking in the natural surroundings.',
+      "Host your large group or multiple families in the expansive and luxurious Wooden Cabin 007. Accommodating up to eight people, this cabin provides a private escape nestled among stunning forests and mountains. Inside, you'll find cozy and inviting interiors crafted from high-quality wood, several living areas with fireplaces, and a fully-equipped kitchen. The bedrooms offer comfort with en-suite bathrooms. Enjoy the private deck with a hot tub and outdoor seating area, ideal for soaking in the natural beauty around you.",
   },
   {
     name: '008',
@@ -73,6 +73,6 @@ export const cabins = [
     discount: 15,
     image: imageUrl + 'cabin-008.jpg',
     description:
-      "Experience the epitome of luxury and grandeur with your large group or multiple families in our grand cabin 008. This cabin offers a lavish retreat that caters to all your needs and desires. The cabin features an opulent design and boasts of high-end finishes, intricate details, and the finest quality wood throughout. Inside, the cabin features multiple grand living areas with fireplaces, a formal dining area, and a gourmet kitchen that is a chef's dream. The bedrooms are designed for ultimate comfort and luxury, with plush beds and en-suite spa-inspired bathrooms. Step outside and immerse yourself in the beauty of nature from your private deck, featuring a luxurious hot tub and ample seating areas for ultimate relaxation and enjoyment.",
+      'Experience unparalleled luxury and grandeur with your large group or multiple families in our magnificent Cabin 008. This retreat provides a lavish escape tailored to meet all your needs and desires. The cabin showcases an opulent design with high-end finishes, intricate details, and the finest quality wood throughout. Inside, you’ll find multiple expansive living areas with fireplaces, a formal dining space, and a gourmet kitchen that’s a chef’s dream. The bedrooms are designed for supreme comfort and luxury, featuring plush beds and spa-inspired en-suite bathrooms. Step outside to your private deck and immerse yourself in nature’s beauty, complete with a luxurious hot tub and ample seating areas for ultimate relaxation and enjoyment.',
   },
 ];
